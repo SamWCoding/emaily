5f8dc2ee465ac950470071e884641e442ebe38e9
Sam Williams <samwcoding@gmail.com>
2017-11-12 12:54:49 +0800

    refactoring for async

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const keys = require("./config/keys");
-require("./models/User");
+require("./models/user");
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
@@ -21,7 +21,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require("./routes/authRoutes")(app);
+require("./routes/auth-routes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
